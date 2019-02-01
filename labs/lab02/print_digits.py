6dae824404e32b7dfb9a840422832a2db585fb0f
Jeffery Liu <liu00614@umn.edu>
2019-02-01 15:26:40 -0600

    improving print_digits.py to print any number of digits

--- labs/lab02/print_digits.py
+++ labs/lab02/print_digits.py
@@ -1,6 +1,4 @@
-n = int(input('Enter a four digit integer: '))
-print(n//1000)
-print(n//100-10*(n//1000))
-print(n//10-10*(n//100))
-print(n-10*(n//10))
-    
+n = int(input('Enter a positive integer: '))
+
+for m in range(0,len(str(n))):
+    print((n//(10**(len(str(n))-m-1)))%10)
